a1042523a2ce7644902d3e460b8ac307275c9668
Fly Dev <dev@fly.io>
2025-06-26 19:54:54 +0000

    add workout notebook route and remove placeholder home

--- lib/rep_rivals_web/router.ex
+++ lib/rep_rivals_web/router.ex
@@ -1,6 +1,8 @@
 defmodule RepRivalsWeb.Router do
   use RepRivalsWeb, :router
 
+  import RepRivalsWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule RepRivalsWeb.Router do
     plug :put_root_layout, html: {RepRivalsWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -16,8 +19,6 @@ defmodule RepRivalsWeb.Router do
 
   scope "/", RepRivalsWeb do
     pipe_through :browser
-
-    get "/", PageController, :home
   end
 
   # Other scopes may use custom stacks.
@@ -40,5 +41,34 @@ defmodule RepRivalsWeb.Router do
       live_dashboard "/dashboard", metrics: RepRivalsWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", RepRivalsWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{RepRivalsWeb.UserAuth, :require_authenticated}] do
+      live "/", WorkoutNotebookLive, :index
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", RepRivalsWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{RepRivalsWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
