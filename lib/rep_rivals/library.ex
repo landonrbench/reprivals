3a085357463c256b0ff80b473b8e2b4bcb5a1c6b
Fly Dev <dev@fly.io>
2025-06-26 19:53:53 +0000

    update Library context to use separate Workout schema

--- lib/rep_rivals/library.ex
+++ lib/rep_rivals/library.ex
@@ -8,8 +8,6 @@ defmodule RepRivals.Library do
 
   alias RepRivals.Library.Workout
 
-  alias __MODULE__.Workout
-
   @doc """
   Returns the list of workouts for a specific user.
 
@@ -105,29 +103,5 @@ defmodule RepRivals.Library do
   """
   def change_workout(%Workout{} = workout, attrs \\ %{}) do
     Workout.changeset(workout, attrs)
-  end
-end
-
-defmodule RepRivals.Library.Workout do
-  use Ecto.Schema
-  import Ecto.Changeset
-
-  schema "workouts" do
-    field :name, :string
-    field :description, :string
-    field :metric, :string
-    belongs_to :user, RepRivals.Accounts.User
-
-    timestamps(type: :utc_datetime)
-  end
-
-  @doc false
-  def changeset(workout, attrs) do
-    workout
-    |> cast(attrs, [:name, :description, :metric, :user_id])
-    |> validate_required([:name, :description, :metric, :user_id])
-    |> validate_inclusion(:metric, ["For Time", "For Reps", "Weight"])
-    |> validate_length(:name, min: 1, max: 100)
-    |> validate_length(:description, min: 1, max: 1000)
   end
 end
