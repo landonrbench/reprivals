80c10fefb0ac7dd279173b5b510dc43b17dd4143
Fly Dev <dev@fly.io>
2025-06-27 20:03:03 +0000

    add friends route to router

--- lib/rep_rivals_web/router.ex
+++ lib/rep_rivals_web/router.ex
@@ -55,6 +55,7 @@ defmodule RepRivalsWeb.Router do
       live "/workouts/new", WorkoutNewLive, :new
       live "/workouts/:id", WorkoutDetailLive, :show
       live "/workouts/:id/edit", WorkoutEditLive, :edit
+      live "/friends", FriendsLive, :index
       live "/users/settings", UserLive.Settings, :edit
       live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
     end
