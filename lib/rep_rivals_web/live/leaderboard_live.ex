e6ec760a4a9204f85961c96883c27bc3c74c78ee
Fly Dev <dev@fly.io>
2025-07-01 21:05:19 +0000

    fix syntax error in format_result_with_unit

--- lib/rep_rivals_web/live/leaderboard_live.ex
+++ lib/rep_rivals_web/live/leaderboard_live.ex
@@ -42,12 +42,13 @@ defmodule RepRivalsWeb.LeaderboardLive do
   defp format_result_with_unit(result_value, result_unit) do
     formatted_value =
       case result_value do
-        result_value
-        |> Decimal.to_string()
-      %Decimal{} ->
-        |> String.replace(~r/\.?0+$/, "")
-      _ ->
-        to_string(result_value)
+        %Decimal{} ->
+          result_value
+          |> Decimal.to_string()
+          |> String.replace(~r/\.?0+$/, "")
+
+        _ ->
+          to_string(result_value)
       end
 
     case result_unit do
