ef73f1b2ea55bcc07dd41c9a82b09866ec586a36
Fly Dev <dev@fly.io>
2025-07-01 22:00:14 +0000

    add missing leaderboard icon to home navigation

--- lib/rep_rivals_web/live/home_live.ex
+++ lib/rep_rivals_web/live/home_live.ex
@@ -168,6 +168,21 @@ defmodule RepRivalsWeb.HomeLive do
             </.link>
 
             <.link
+              navigate={~p"/leaderboard"}
+              class="flex flex-col items-center py-2 px-4 text-gray-400 hover:text-orange-500 transition-colors"
+            >
+              <svg class="w-6 h-6 mb-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                />
+              </svg>
+              <span class="text-xs">Leaderboard</span>
+            </.link>
+
+            <.link
               navigate={~p"/friends"}
               class="flex flex-col items-center py-2 px-4 text-gray-400 hover:text-orange-500 transition-colors"
             >
