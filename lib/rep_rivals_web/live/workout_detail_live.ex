7b5d0b03a2d37a227bbcdb1c2d9e0c509848f5e4
Fly Dev <dev@fly.io>
2025-07-02 16:09:07 +0000

    add challenge friends functionality to workout detail live

--- lib/rep_rivals_web/live/workout_detail_live.ex
+++ lib/rep_rivals_web/live/workout_detail_live.ex
@@ -27,12 +27,16 @@ defmodule RepRivalsWeb.WorkoutDetailLive do
      |> assign(:show_edit_result_form, false)
      |> assign(:show_delete_result_modal, false)
      |> assign(:show_challenge_modal, false)
+     |> assign(:show_challenge_friends_modal, false)
      |> assign(:current_result, nil)
      |> assign(:challenge_result, nil)
      |> assign(:selected_participant_ids, [])
+     |> assign(:available_friends, [])
+     |> assign(:selected_challenge_friends, [])
      |> assign(:form, to_form(Library.change_workout_result(%WorkoutResult{})))
      |> assign(:edit_form, to_form(Library.change_workout(workout)))
-     |> assign(:edit_result_form, to_form(Library.change_workout_result(%WorkoutResult{})))}
+     |> assign(:edit_result_form, to_form(Library.change_workout_result(%WorkoutResult{})))
+     |> assign(:challenge_form, to_form(%{}))}
   end
 
   @impl true
@@ -84,6 +88,107 @@ defmodule RepRivalsWeb.WorkoutDetailLive do
 
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, form: to_form(changeset))}
+    end
+  end
+
+  @impl true
+  def handle_event("log_result_and_challenge", _params, socket) do
+    # First validate and create the workout result
+    workout = socket.assigns.workout
+    user_id = socket.assigns.current_scope.user.id
+    form_data = Phoenix.HTML.Form.params(socket.assigns.form)
+    result_params = form_data["workout_result"] || %{}
+
+    # Parse the date and set it as the logged_at datetime
+    logged_at =
+      case result_params["logged_at"] do
+        date_string when is_binary(date_string) and date_string != "" ->
+          case Date.from_iso8601(date_string) do
+            {:ok, date} -> DateTime.new!(date, ~T[12:00:00])
+            _ -> DateTime.utc_now()
+          end
+
+        _ ->
+          DateTime.utc_now()
+      end
+
+    result_params =
+      result_params
+      |> Map.put("workout_id", workout.id)
+      |> Map.put("user_id", user_id)
+      |> Map.put("logged_at", logged_at)
+
+    case Library.create_workout_result(result_params) do
+      {:ok, workout_result} ->
+        # Get available friends (all users except current user)
+        available_friends = Enum.reject(socket.assigns.users, &(&1.id == user_id))
+
+        {:noreply,
+         socket
+         |> assign(show_log_form: false)
+         |> assign(show_challenge_friends_modal: true)
+         |> assign(available_friends: available_friends)
+         |> assign(selected_challenge_friends: [])
+         |> assign(challenge_form: to_form(%{"name" => "", "description" => ""}))
+         |> assign(challenge_result: workout_result)
+         |> put_flash(:info, "Result logged! Now create a challenge for your friends.")}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, form: to_form(changeset))}
+    end
+  end
+
+  @impl true
+  def handle_event("hide_challenge_friends_modal", _params, socket) do
+    {:noreply,
+     socket
+     |> assign(show_challenge_friends_modal: false)
+     |> assign(available_friends: [])
+     |> assign(selected_challenge_friends: [])
+     |> assign(challenge_result: nil)}
+  end
+
+  @impl true
+  def handle_event("toggle_challenge_friend", %{"id" => friend_id}, socket) do
+    friend_id = String.to_integer(friend_id)
+    current_selected = socket.assigns.selected_challenge_friends
+
+    updated_selected =
+      if friend_id in current_selected do
+        List.delete(current_selected, friend_id)
+      else
+        [friend_id | current_selected]
+      end
+
+    {:noreply, assign(socket, selected_challenge_friends: updated_selected)}
+  end
+
+  @impl true
+  def handle_event("create_challenge_with_result", %{"name" => name, "description" => description}, socket) do
+    workout = socket.assigns.workout
+    user_id = socket.assigns.current_scope.user.id
+    selected_friends = socket.assigns.selected_challenge_friends
+
+    challenge_attrs = %{
+      name: name,
+      description: description,
+      status: "active",
+      creator_id: user_id,
+      workout_id: workout.id
+    }
+
+    case Library.create_group_challenge(challenge_attrs, selected_friends) do
+      {:ok, _challenge} ->
+        {:noreply,
+         socket
+         |> assign(show_challenge_friends_modal: false)
+         |> assign(available_friends: [])
+         |> assign(selected_challenge_friends: [])
+         |> assign(challenge_result: nil)
+         |> put_flash(:info, "Challenge created and friends invited!")}
+
+      {:error, _changeset} ->
+        {:noreply, put_flash(socket, :error, "Failed to create challenge")}
     end
   end
 
@@ -188,7 +293,7 @@ defmodule RepRivalsWeb.WorkoutDetailLive do
         {:noreply,
          socket
          |> assign(:show_delete_result_modal, false)
-         |> put_flash(:error, "Failed to delete result")}
+         |> put_flash(:error, "Failed to delete result"))
     end
   end
 
@@ -298,7 +403,7 @@ defmodule RepRivalsWeb.WorkoutDetailLive do
         {:noreply,
          socket
          |> assign(show_delete_modal: false)
-         |> put_flash(:error, "Failed to delete workout")}
+         |> put_flash(:error, "Failed to delete workout"))
     end
   end
 
@@ -381,5 +486,9 @@ defmodule RepRivalsWeb.WorkoutDetailLive do
 
   defp user_is_selected?(user_id, selected_ids) do
     user_id in selected_ids
+  end
+
+  defp friend_selected_for_challenge?(friend_id, selected_friends) do
+    friend_id in selected_friends
   end
 end
